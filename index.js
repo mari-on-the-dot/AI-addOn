14e62aad6c243f771609afabb05e78bda37166f6
mari-on-the-dot <mrnb88@gmail.com>
2025-05-10 18:20:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/* function showResults(response) {
+  alert(response.data.answer);
+}
+
+let apiKey = "2f76b36a73748t15dd07ebbo0f40f91a";
+let context = "You enjoy reading science fiction. Be concise in your answers";
+let prompt = "What is your absolutely favorite book/saga";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("processing");
+axios.get(apiUrl).then(showResults);
+*/
