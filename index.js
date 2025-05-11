4e7cd4a53d46928626bed1099ea52eb113edf0ab
mari-on-the-dot <mrnb88@gmail.com>
2025-05-11 15:42:05 +0200

    added api integration  to get ai generated poem. JS polished

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${
 console.log("processing");
 axios.get(apiUrl).then(showResults);
 */
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+  });
+}
 
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "o silencio na rua",
-    autoStart: true,
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let userText = instructionsInput.value;
+
+  let apiKey = "2f76b36a73748t15dd07ebbo0f40f91a";
+
+  let prompt =
+    "Your mission is to generate a 4 line poem, Each line should be on its own line using <br> tags. Make sure to follow the user instructions";
+  let context = `User instructions: Generate a short poem in portuguese about ${userText}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
